79e40e9ce85c96bad5ada79cba6a62864dca71bb
Aaron <aaron.ch.balan@gmail.com>
2020-06-28 19:18:03 +0100

    testing a change

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,12 +1,11 @@
 n = 1000
-q  =
-p = 
+q  = 3
+p = 5
 x = [i for i in range(1, n + 1)]
 
-x[q - 1::3] = ["fizz" for j in range(len(x[2::3]))]
-x[4::5] = ["buzz" for k in range(len(x[4::5]))]
-x[14::15] = ["fizzbuzz" for l in range(len(x[14::15]))]
+x[(q - 1)::q] = ["fizz" for j in range(len(x[2::3]))]
+x[(p - 1)::p] = ["buzz" for k in range(len(x[4::5]))]
+x[(q * p - 1)::(q * p)] = ["fizzbuzz" for l in range(len(x[14::15]))]
 
 for m in x:
     print(m)
-#hello, this is a change
