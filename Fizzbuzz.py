2862cdb89f4eb0a810c402d81e73e782de7e7840
Aaron <aaron.ch.balan@gmail.com>
2020-06-28 19:37:47 +0100

    My first code

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,11 +1,8 @@
-n = 1000
-q  = 3
-p = 5
-x = [i for i in range(1, n + 1)]
+x = [i for i in range(1, 101)]
 
-x[(q - 1)::q] = ["fizz" for j in range(len(x[2::3]))]
-x[(p - 1)::p] = ["buzz" for k in range(len(x[4::5]))]
-x[(q * p - 1)::(q * p)] = ["fizzbuzz" for l in range(len(x[14::15]))]
+x[2::3] = ["fizz" for j in range(len(x[2::3]))]
+x[4::5] = ["buzz" for k in range(len(x[4::5]))]
+x[14::15] = ["fizzbuzz" for l in range(len(x[14::15]))]
 
 for m in x:
     print(m)
