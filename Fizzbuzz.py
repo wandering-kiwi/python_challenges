21b920527d0319463fa95264e8f4453246a00840
Aaron <avni_aaron@Avnis-MacBook-Air.local>
2020-06-28 17:53:28 +0100

    Initional commit

---
+++ Fizzbuzz.py
@@ -0,0 +1,11 @@
+n = 1000
+q  =
+p = 
+x = [i for i in range(1, n + 1)]
+
+x[q - 1::3] = ["fizz" for j in range(len(x[2::3]))]
+x[4::5] = ["buzz" for k in range(len(x[4::5]))]
+x[14::15] = ["fizzbuzz" for l in range(len(x[14::15]))]
+
+for m in x:
+    print(m)
