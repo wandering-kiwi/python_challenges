4b4da4593f27c45da044786da8704fb4a39ce4e4
wandering-kiwi <67548758+wandering-kiwi@users.noreply.github.com>
2024-08-07 15:43:43 +0100

    Update Fizzbuzz1.py

--- Fizzbuzz/Fizzbuzz1.py
+++ Fizzbuzz/Fizzbuzz1.py
@@ -3,7 +3,6 @@
 """
 Created on Sun Jun 28 11:58:40 2020
 
-@author: avni_aaron
 """
 
 print([("fizzbuzz" if (i%5==0) else ("fizz")) if (i%3==0) else ("buzz" if (i%5==0) else (i)) for i in range(1, 101)])
